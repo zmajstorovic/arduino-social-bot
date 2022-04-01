0b17592afa3fdb82985bb1498741b040be9b77a7
zmajstorovic <majstorovic_zeljko@hotmail.com>
2022-04-01 22:28:41 +0200

    first post published

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from turtle import st
+from matplotlib.pyplot import pause
 import serial
 import time
+import tweepy
+import json
 
 def getTemp():
     # make sure the 'COM#' is set according the Windows Device Manager
@@ -14,5 +17,45 @@ def getTemp():
     return string
     ser.close()
 
-T = getTemp()
-print(T)
+def TwitterAuth():
+
+    with open('config.json') as json_file:
+        data = json.load(json_file)
+    
+    #print(data['consumer_key'])
+    twitter_auth_keys = {
+        "consumer_key"        : data['consumer_key'],
+        "consumer_secret"     : data['consumer_secret'],
+        "access_token"        : data['access_token'],
+        "access_token_secret" : data['access_token_secret']
+    }
+
+    auth = tweepy.OAuthHandler(
+            twitter_auth_keys['consumer_key'],
+            twitter_auth_keys['consumer_secret']
+            )
+    auth.set_access_token(
+            twitter_auth_keys['access_token'],
+            twitter_auth_keys['access_token_secret']
+            )
+    api = tweepy.API(auth)
+    return api
+
+def main():
+
+   
+    #api = tweepy.api
+    while(True):
+    
+        api = TwitterAuth()
+        temp = getTemp()
+        tweet = "Izmjerena temperatura u uredu je "+temp[3:]+"°C, a vlažnost zraka je "+temp[0:2]+"%."
+        status = api.update_status(status=tweet)
+        print(temp[0:2]+'   '+temp[3:])
+
+        
+        pause(3600)
+    
+
+if __name__ == "__main__":
+    main()
